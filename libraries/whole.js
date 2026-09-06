c383afa8d5380a7f4036519e3092ef7e40cec24a
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-06 11:07:58 +0200

    Here.

---
+++ libraries/whole.js
@@ -0,0 +1,9 @@
+document.querySelector('#latest').addEventListener('click', () => {
+    location.href="https://arancia-3-open-services.github.io/store/latest"
+});
+document.querySelector('#reload').addEventListener('click', () => {
+    location.href="./index.html"
+});
+document.querySelector('#libs').addEventListener('click', () => {
+    location.href="https://arancia-3-open-services.github.io/store/libraries"
+});
