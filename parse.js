e6dc0321e0deea201655aefdb996001282d032c2
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-09 10:17:35 +0200

    Here.

--- parse.js
+++
@@ -1,6 +0,0 @@
-// Converts the content.
-const formatted = JSON.parse(
-    '{"aoo":"SCAM ALERT!!!"}'
-);
-let aoo = formatted.aoo;
-console.log(aoo + " Do not paste things into here that other people sent you!")
