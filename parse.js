eb870fd875446afad66e87c0021cb71d24abeb4c
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-09 10:12:41 +0200

    A pack.

---
+++ parse.js
@@ -0,0 +1,6 @@
+// Converts the content.
+const formatted = JSON.parse(
+    '{"aoo":"SCAM ALERT!!!"}'
+);
+let aoo = formatted.aoo;
+console.log(aoo + " Do not paste things into here that other people sent you!")
